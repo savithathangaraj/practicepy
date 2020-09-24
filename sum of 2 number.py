d2385ca9d569615d7d13fba18b2f58fdd824b86e
savitha <62764314+savithathangaraj@users.noreply.github.com>
2020-09-24 16:14:51 +0530

    subarray

--- sum of 2 number.py
+++ sum of 2 number.py
@@ -2,4 +2,24 @@
 a=3
 b=8
 sum=a+b
-print(sum)
+
+#subarrays of an array
+
+list=[1,2,3,4]
+j=0
+count=0
+for i in range(len(list)):
+    if i<=j:
+        
+        print("Hello World{",list[i],"}")
+        count+=1
+    j+=1
+    
+for i in range(0,len(list),1):
+    print("Hello World {",list[i],list[i+1],"}")
+    count+=1
+i=0
+for i in range(0,len(list),1):
+    print("hello world {",list[i],list[i+1],list[i+2],"}")
+    count+=1
+   
