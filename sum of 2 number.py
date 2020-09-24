a528d3410eff522a155b6f0b9cd7319129f18951
savitha <62764314+savithathangaraj@users.noreply.github.com>
2020-09-24 16:37:57 +0530

    parellel iteration of 2 list

--- sum of 2 number.py
+++ sum of 2 number.py
@@ -22,4 +22,19 @@ i=0
 for i in range(0,len(list),1):
     print("hello world {",list[i],list[i+1],list[i+2],"}")
     count+=1
-   
+
+# iterating 2 list in same time and returning as list
+ c=0
+    d=0
+    for (i,j) in zip(a,b):
+        if i>j:
+            c+=1
+        elif i<j:
+            d+=1
+        else:
+            pass
+    e=[c,d]
+    # e[0]=c
+    # e[1]=d
+
+    return e
