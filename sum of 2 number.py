64f52ae1f0e2f8324430eed25bd370ac9c715966
savitha <62764314+savithathangaraj@users.noreply.github.com>
2020-09-25 15:48:16 +0530

    jumping on clouds

--- sum of 2 number.py
+++ sum of 2 number.py
@@ -91,3 +91,10 @@ def plusMinus(arr):
         k-=1
         j+=1
         print()
+#jumping on clouds problem
+    if len(c) == 1 : return 0
+    if len(c) == 2: return 0 if c[1]==1 else 1
+    if c[2]==1: 
+        return 1 + jumpingOnClouds(c[1:])
+    if c[2]==0:
+        return 1 + jumpingOnClouds(c[2:])
